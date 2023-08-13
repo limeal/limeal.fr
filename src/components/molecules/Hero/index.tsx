9111c62932d3ed446bcaeac57e3c8cff28971c33
BuildTools <paul.gazeau-rousseau@epitech.eu>
2023-08-13 23:23:04 +0200

    fix: Correct visual problem in responsive mod

--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Tabs from "@/components/atomes/Tabs";
 import "./style.scss";
 import getTranslation from "@/utils/lang";
@@ -7,11 +9,41 @@ import { useAuthContext } from "@/contexts/AuthContext";
 import { BsBookHalf } from "react-icons/bs";
 import { useRouter } from "next/navigation";
 import { defaultTabs } from "@/utils/constant";
+import { useEffect, useRef, useState } from "react";
 
 const Hero = ({ lang }: { lang?: string }) => {
-
   const { user } = useAuthContext();
   const router = useRouter();
+
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  const ref = useRef<HTMLUListElement>(null);
+
+  const minSwipeDistance = 40;
+
+  const onTouchStart = (e: any) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e: any) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    if (!ref.current) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    const size = parseInt(ref.current.style.right.replace(/[^0-9]/g, ""));
+
+    if (isLeftSwipe) {
+      // Move to left
+      ref.current.style.right = size + distance > 1166 ? "1166px" : size + distance + "px";
+    } else if (isRightSwipe) {
+      ref.current.style.right = 
+        size + distance < -198 ? "-198px" : size + distance + "px";
+    }
+  };
 
   return (
     <section className="hero" id="about-me">
@@ -80,29 +112,46 @@ const Hero = ({ lang }: { lang?: string }) => {
                 {
                   href: "#contact-me",
                   tid: "tabs--contact-me",
-                  type: "link"
-                }
+                  type: "link",
+                },
               ]}
             />
           </div>
           <div className="hero__content__right">
             <div>
-              <Image sizes="100vw" className="hero__content__right__man" src="/assets/images/businessmann.png" alt="hero-image" width={0} height={0} />
+              <Image
+                sizes="100vw"
+                className="hero__content__right__man"
+                src="/assets/images/businessmann.png"
+                alt="hero-image"
+                width={0}
+                height={0}
+              />
               <Image
                 sizes="100vw"
                 className="hero__content__right__head"
                 src="/assets/images/logo.png"
                 alt="head"
-                width={0} height={0}
+                width={0}
+                height={0}
               />
               <div>
-                <Image sizes="100vw" style={{width: "100%", height: "100%" }} src="/assets/images/icons/arrow_spiral.svg" alt="quote" width={0} height={0} />
+                <Image
+                  sizes="100vw"
+                  style={{ width: "100%", height: "100%" }}
+                  src="/assets/images/icons/arrow_spiral.svg"
+                  alt="quote"
+                  width={0}
+                  height={0}
+                />
                 <span>
                   {user ? (
                     <button onClick={() => router.push("/blog")}>
                       <BsBookHalf />
                     </button>
-                  ) : getTranslation(lang || "en", "hero--image--quote")}
+                  ) : (
+                    getTranslation(lang || "en", "hero--image--quote")
+                  )}
                 </span>
               </div>
             </div>
@@ -110,20 +159,26 @@ const Hero = ({ lang }: { lang?: string }) => {
           </div>
         </div>
       </div>
-      <ul>
+      <ul
+        ref={ref}
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+        style={{
+          right: "598px",
+          left: "initial",
+        }}
+      >
         {Array(6)
           .fill(0)
           .map((_, index) => (
-            <li
-              key={index}
-            >
+            <li key={index}>
               <Image
                 sizes="100vw"
                 src={`/assets/images/logos/${index + 1}.png`}
                 alt={`fc-${index + 1}`}
                 width={0}
                 height={0}
-                style={{width: "100%", height: "100%" }}
               />
             </li>
           ))}
